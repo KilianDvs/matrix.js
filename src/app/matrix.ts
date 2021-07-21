3df683efad89f871c3fca4347ba03f5be69fa7ca
CamilleAbella <camille.abella@hotmail.fr>
2021-07-21 15:21:48 +0200

    add remove functions

--- src/app/matrix.ts
+++ src/app/matrix.ts
@@ -8,9 +8,9 @@ export interface Cell<Data> {
 }
 
 export class Matrix<Data> {
-  private input: Table<Data> = []
-  private width: number = null
-  private height: number = null
+  private _input: Table<Data> = []
+  private _width: number = null
+  private _height: number = null
 
   constructor(input: Values<Data>, private asColumns = false) {
     if (input.length === 0) return
@@ -20,77 +20,113 @@ export class Matrix<Data> {
   }
 
   get rows(): Table<Data> {
-    return this.input.slice()
+    return this._input
   }
 
   get cols(): Table<Data> {
     const cols: Table<Data> = []
 
-    for (let x = 0; x < this.width; x++) cols.push(this.getCol(x))
+    for (let x = 0; x < this._width; x++) cols.push(this.getCol(x))
 
     return cols
   }
 
   get values(): Values<Data> {
-    return this.input.map((row) => {
+    return this._input.map((row) => {
       return row.map((cell) => cell.data)
     })
   }
 
   getRow(y: number): Row<Data> {
-    return this.input[y]
+    return this._input[y]
   }
 
   getCol(x: number): Col<Data> {
-    return this.input.map((row) => row[x])
+    return this._input.map((row) => row[x])
   }
 
   forRows(fn: (row: Row<Data>, y: number, rows: Table<Data>) => unknown) {
-    this.input.forEach(fn)
+    this.rows.forEach(fn)
+
+    return this
   }
 
-  forCols(fn: (col: Col<Data>, y: number, rows: Table<Data>) => unknown) {
-    this.input.forEach(fn)
+  forCols(fn: (col: Col<Data>, x: number, cols: Table<Data>) => unknown) {
+    this.cols.forEach(fn)
+
+    return this
+  }
+
+  removeRow(at?: number) {
+    if (this._height === undefined || this._height === 0) return
+
+    this._height--
+
+    if (at === undefined) this._input.pop()
+    else this._input.splice(at, 1)
+
+    return this
+  }
+
+  removeCol(at?: number) {
+    if (this._width === undefined || this._width === 0) return
+
+    this._width--
+
+    this._input.forEach((row) => {
+      if (at === undefined) row.pop()
+      else row.splice(at, 1)
+    })
+
+    return this
   }
 
   addRow(input: Data[], at?: number) {
     const width = input.length
 
-    if (this.width !== null && this.width !== width)
-      throw new Error("Invalid row length, expected width: " + this.width)
+    if (this._width !== null && this._width !== width)
+      throw new Error("Invalid row length, expected width: " + this._width)
 
-    this.width = width
-    this.height++
+    this._width = width
+    this._height++
 
     const row: Row<Data> = input.map((data) => ({ data }))
 
-    if (at === undefined) this.input.push(row)
-    else this.input.splice(at, 0, row)
+    if (at === undefined) this._input.push(row)
+    else this._input.splice(at, 0, row)
+
+    return this
   }
 
   addCol(input: Data[], at?: number) {
     const height = input.length
 
-    if (this.height !== null && this.height !== height)
-      throw new Error("Invalid col length, expected height: " + this.width)
+    if (this._height !== null && this._height !== height)
+      throw new Error("Invalid col length, expected height: " + this._width)
 
-    this.height = height
-    this.width++
+    this._height = height
+    this._width++
 
     const col: Col<Data> = input.map((data) => ({ data }))
 
-    this.input.forEach((row, i) => {
+    this._input.forEach((row, i) => {
       if (at === undefined) row.push(col[i])
       else row.splice(at, 0, col[i])
     })
+
+    return this
   }
 
   addRows(...input: Data[][]) {
     for (const row of input) this.addRow(row)
+
+    return this
   }
 
   addCols(...input: Data[][]) {
     for (const col of input) this.addCol(col)
+
+    return this
   }
 
   clone() {
