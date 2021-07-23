a4c565d6e59498be54f81e57a8e0b7ee01f472e5
energistix <50068022+energistix@users.noreply.github.com>
2021-07-23 11:13:34 +0200

    adding some tests

--- test.js
+++ test.js
@@ -1,6 +1,167 @@
+const { Matrix } = require(".")
 
-const lib = require(".")
+let matrixOne = new Matrix([
+  ["John", "Doe", 34, false, false],
+  ["Lisa", "Simpson", 10, true, false],
+  ["Sherlock", "Holmes", 42, false, true],
+  ["Kilian", "Davies", 21, false, true]
+])
 
-test("hello world", () => {
-  expect(lib.hello()).toBe("Hello World!");
+test("Matrix#cols", () => {
+  expect(matrixOne.cols).toStrictEqual([
+    [
+      { data: "John" },
+      { data: "Lisa" },
+      { data: "Sherlock" },
+      { data: "Kilian" }
+    ],
+    [
+      { data: "Doe" },
+      { data: "Simpson" },
+      { data: "Holmes" },
+      { data: "Davies" }
+    ],
+    [{ data: 34 }, { data: 10 }, { data: 42 }, { data: 21 }],
+    [{ data: false }, { data: true }, { data: false }, { data: false }],
+    [{ data: false }, { data: false }, { data: true }, { data: true }]
+  ])
+})
+
+test("Matrix#rows", () => {
+  expect(matrixOne.rows).toStrictEqual([
+      [
+        { data: "John" },
+        { data: "Doe" },
+        { data: 34 },
+        { data: false },
+        { data: false }
+      ],
+      [
+        { data: "Lisa" },
+        { data: "Simpson" },
+        { data: 10 },
+        { data: true },
+        { data: false }
+      ],
+      [
+        { data: "Sherlock" },
+        { data: "Holmes" },
+        { data: 42 },
+        { data: false },
+        { data: true }
+      ],
+      [
+        { data: "Kilian" },
+        { data: "Davies" },
+        { data: 21 },
+        { data: false },
+        { data: true }
+      ]
+    ]
+  )
+})
+
+test("Matrix#width", () => {
+  expect(matrixOne.width).toBe(5)
+})
+
+test("Matrix#height", () => {
+  expect(matrixOne.height).toBe(4)
+})
+
+test("Matrix#removeCol", () => {
+  matrixOne.removeCol(0)
+  expect(matrixOne.cols).toStrictEqual([
+      [
+        { data: "Doe" },
+        { data: "Simpson" },
+        { data: "Holmes" },
+        { data: "Davies" }
+      ],
+      [{ data: 34 }, { data: 10 }, { data: 42 }, { data: 21 }],
+      [{ data: false }, { data: true }, { data: false }, { data: false }],
+      [{ data: false }, { data: false }, { data: true }, { data: true }]
+    ]
+  )
+})
+
+test("Matrix#addCol", () => {
+  matrixOne.addCol(["John", "Lisa", "Sherlock", "Kilian"], 0)
+  expect(matrixOne.cols).toStrictEqual([[
+      { data: "John" },
+      { data: "Lisa" },
+      { data: "Sherlock" },
+      { data: "Kilian" }
+    ],
+    [
+      { data: "Doe" },
+      { data: "Simpson" },
+      { data: "Holmes" },
+      { data: "Davies" }
+    ],
+    [{ data: 34 }, { data: 10 }, { data: 42 }, { data: 21 }],
+    [{ data: false }, { data: true }, { data: false }, { data: false }],
+    [{ data: false }, { data: false }, { data: true }, { data: true }]])
+})
+
+test("Matrix#removeRow", ()=>{
+  matrixOne.removeRow(0)
+  expect(matrixOne.rows).toStrictEqual([
+      [
+        { data: 'Lisa' },
+        { data: 'Simpson' },
+        { data: 10 },
+        { data: true },
+        { data: false }
+      ],
+      [
+        { data: 'Sherlock' },
+        { data: 'Holmes' },
+        { data: 42 },
+        { data: false },
+        { data: true }
+      ],
+      [
+        { data: 'Kilian' },
+        { data: 'Davies' },
+        { data: 21 },
+        { data: false },
+        { data: true }
+      ]
+    ]
+  )
+})
+
+test("Matrix#addRow", ()=>{
+  matrixOne.addRow(["John", "Doe", 34, false, false],0)
+  expect(matrixOne.rows).toStrictEqual([
+    [
+      { data: "John" },
+      { data: "Doe" },
+      { data: 34 },
+      { data: false },
+      { data: false }
+    ],
+    [
+      { data: "Lisa" },
+      { data: "Simpson" },
+      { data: 10 },
+      { data: true },
+      { data: false }
+    ],
+    [
+      { data: "Sherlock" },
+      { data: "Holmes" },
+      { data: 42 },
+      { data: false },
+      { data: true }
+    ],
+    [
+      { data: "Kilian" },
+      { data: "Davies" },
+      { data: 21 },
+      { data: false },
+      { data: true }
+    ]
+  ])
 })
